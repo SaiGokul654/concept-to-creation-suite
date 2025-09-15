51ad71a450c794c51894bff0a3696a70b8bd3c2b
Sai Gokul <saigokul850@gmail.com>
2025-09-15 20:40:28 +0530

    Changed a lot of things

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourname.concepttocreationsuite',
+  appName: 'ConceptToCreationSuite',
+  webDir: 'dist'
+};
+
+export default config;
